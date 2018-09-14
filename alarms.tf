0577581081024db93274b792cb3215d6f63978f0
Michael Kuhinica <taverneiro@gmail.com>
2018-09-14 14:49:44 +1200

    Add extra filters to comply with CIS AWS benchmark (#7)
    * Add filter for sign in with no MFA
    
    * Run build-harness/vendor/terraform fmt
    
    * Add period at the end of no MFA alert description
    
    * Fix no MFA dashboard layout y value
    
    * Add root account usage filter, alarm and widget
    
    * Add KMS key deletion filter, alarm and widget
    
    Source: https://docs.aws.amazon.com/kms/latest/developerguide/deleting-keys-creating-cloudwatch-alarm.html
    
    * Add AWS config changes filter, alarm and widget
    
    * Add route table changes filter, alert and widget
    
    * Fix alarm description
    
    * Add new metrics to the README
    
    * Update available targets, autogenerated
    
    * Add one single (whole) period.

--- alarms.tf
+++ alarms.tf
@@ -19,6 +19,11 @@ locals {
     "CloudTrailEventCount",
     "ConsoleSignInFailureCount",
     "IAMPolicyEventCount",
+    "ConsoleSignInWithoutMfaCount",
+    "RootAccountUsageCount",
+    "KMSKeyPendingDeletionErrorCount",
+    "AWSConfigChangeCount",
+    "RouteTableChangesCount",
   ]
 
   metric_namespace = "${var.metric_namespace}"
@@ -36,6 +41,11 @@ locals {
     "{ ($.eventName = CreateTrail) || ($.eventName = UpdateTrail) || ($.eventName = DeleteTrail) || ($.eventName = StartLogging) || ($.eventName = StopLogging) }",
     "{ ($.eventName = ConsoleLogin) && ($.errorMessage = \"Failed authentication\") }",
     "{ ($.eventName = DeleteGroupPolicy) || ($.eventName = DeleteRolePolicy) ||($.eventName=DeleteUserPolicy)||($.eventName=PutGroupPolicy)||($.eventName=PutRolePolicy)||($.eventName=PutUserPolicy)||($.eventName=CreatePolicy)||($.eventName=DeletePolicy)||($.eventName=CreatePolicyVersion)||($.eventName=DeletePolicyVersion)||($.eventName=AttachRolePolicy)||($.eventName=DetachRolePolicy)||($.eventName=AttachUserPolicy)||($.eventName=DetachUserPolicy)||($.eventName=AttachGroupPolicy)||($.eventName=DetachGroupPolicy)}",
+    "{ $.eventName = \"ConsoleLogin\" && $.additionalEventData.MFAUsed = \"No\" }",
+    "{ $.userIdentity.type = \"Root\" && $.userIdentity.invokedBy NOT EXISTS && $.eventType != \"AwsServiceEvent\" }",
+    "{ $.eventSource = kms* && $.errorMessage = \"* is pending deletion.\"}",
+    "{ $.eventSource = config.amazonaws.com && (($.eventName=StopConfigurationRecorder)||($.eventName=DeleteDeliveryChannel) ||($.eventName=PutDeliveryChannel)||($.eventName=PutConfigurationRecorder)) }",
+    "{ ($.eventName = CreateRoute) || ($.eventName = CreateRouteTable) || ($.eventName = ReplaceRoute) || ($.eventName = ReplaceRouteTableAssociation) || ($.eventName = DeleteRouteTable) || ($.eventName = DeleteRoute) || ($.eventName = DisassociateRouteTable) }",
   ]
 
   alarm_description = [
@@ -50,6 +60,11 @@ locals {
     "Alarms when an API call is made to create, update or delete a .cloudtrail. trail, or to start or stop logging to a trail.",
     "Alarms when an unauthenticated API call is made to sign into the console.",
     "Alarms when an API call is made to change an IAM policy.",
+    "Alarms when a user logs into the console without MFA.",
+    "Alarms when a root account usage is detected.",
+    "Alarms when a customer created KMS key is pending deletion.",
+    "Alarms when AWS Config changes.",
+    "Alarms when route table changes are detected.",
   ]
 }
 
@@ -95,7 +110,7 @@ resource "aws_cloudwatch_dashboard" "main" {
           "width":20,
           "height":16,
           "properties":{
-             "metrics":[ 
+             "metrics":[
                ${join(",",formatlist("[ \"${local.metric_namespace}\", \"%v\" ]", local.metric_name))}
              ],
              "period":300,
@@ -133,7 +148,7 @@ resource "aws_cloudwatch_dashboard" "main_individual" {
           \"title\":\"%v\"
           }
        }
-       ", local.layout_x, local.layout_y, local.metric_name, local.metric_name))}    
+       ", local.layout_x, local.layout_y, local.metric_name, local.metric_name))}
    ]
  }
  EOF
@@ -141,6 +156,8 @@ resource "aws_cloudwatch_dashboard" "main_individual" {
 
 locals {
   # Two Columns
-  layout_x = [0, 12, 0, 12, 0, 12, 0, 12, 0, 12, 0]
-  layout_y = [0, 0, 7, 7, 15, 15, 22, 22, 29, 29, 36]
+  # Will experiment with this values
+  layout_x = [0, 12, 0, 12, 0, 12, 0, 12, 0, 12, 0, 12, 0, 12, 0, 12]
+
+  layout_y = [0, 0, 7, 7, 15, 15, 22, 22, 29, 29, 36, 36, 43, 43, 50, 50]
 }
