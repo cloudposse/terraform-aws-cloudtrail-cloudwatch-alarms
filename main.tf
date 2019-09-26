fb4226edbcb15c6c92d6549bb0906d3ec3cb8d14
Rory Kiefer <2141215+kieferrj@users.noreply.github.com>
2019-09-26 12:04:31 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -53,7 +53,7 @@ data "aws_iam_policy_document" "sns_topic_policy" {
     resources = ["${local.sns_topic_arn}"]
 
     principals {
-      type        = "*"
+      type        = "AWS"
       identifiers = ["*"]
     }
 
