f5fc0253dd4cb42c8a5438b2364be60881176bfc
Jacob Pleiness <pleiness@umich.edu>
2019-10-15 13:41:21 -0400

    Update syntax to terraform 0.12 style
    - Use `null` instead of empty strings
    - Change to `bool` value types where appropriate

--- variables.tf
+++ variables.tf
@@ -1,37 +1,40 @@
 variable "additional_endpoint_arns" {
   description = "Any alert endpoints, such as autoscaling, or app scaling endpoint arns that will respond to an alert"
   default     = []
-  type        = "list"
+  type        = list(string)
 }
 
 variable "sns_topic_arn" {
   description = "An SNS topic ARN that has already been created. Its policy must already allow access from CloudWatch Alarms, or set `add_sns_policy` to `true`"
-  default     = ""
-  type        = "string"
+  default     = null
+  type        = string
 }
 
 variable "add_sns_policy" {
   description = "Attach a policy that allows the notifications through to the SNS topic endpoint"
-  default     = "false"
-  type        = "string"
+  default     = false
+  type        = string
 }
 
 variable "region" {
   description = "The region that should be monitored for unauthorised AWS API Access. Current region used if none provied."
-  default     = ""
-  type        = "string"
+  default     = null
+  type        = string
 }
 
 variable "log_group_name" {
   description = "The cloudtrail cloudwatch log group name"
+  type        = string
 }
 
 variable "metric_namespace" {
   description = "A namespace for grouping all of the metrics together"
   default     = "CISBenchmark"
+  type        = string
 }
 
 variable "create_dashboard" {
   description = "When true a dashboard that displays the statistics as a line graph will be created in CloudWatch"
-  default     = "true"
+  default     = true
+  type        = bool
 }
