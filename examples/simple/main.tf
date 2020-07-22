dc0b1ec24a405d97439fdf1274a658db09755020
Nigel Gibbs <nigel@gibbsoft.com>
2020-07-22 11:34:41 +0100

    Initial terraform 0.12 version
    Tested with terraform 0.12.28

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -6,7 +6,7 @@ variable "region" {
 data "aws_caller_identity" "current" {}
 
 provider "aws" {
-  region = "${var.region}"
+  region = var.region
 
   # Make it faster by skipping something
   skip_get_ec2_platforms      = true
@@ -22,57 +22,57 @@ module "cloudtrail_api_alarms" {
   # source         = "git::https://github.com/cloudposse/terraform-aws-cloudtrail-cloudwatch-alarms.git?ref=master"
   source = "../../"
 
-  region         = "${var.region}"
-  log_group_name = "${aws_cloudwatch_log_group.default.name}"
+  region         = var.region
+  log_group_name = aws_cloudwatch_log_group.default.name
 }
 
 ## Everything after this is standard cloudtrail setup
 
-## When destroying this example, if you want to keep the logs you will need to 
+## When destroying this example, if you want to keep the logs you will need to
 ## Copy the contents of the bucket somewhere first.
 resource "aws_s3_bucket" "default" {
   bucket_prefix = "cw-bucket-${var.region}"
-  region        = "${var.region}"
+  region        = var.region
 
   force_destroy = true
 }
 
 resource "aws_s3_bucket_policy" "default" {
-  bucket = "${aws_s3_bucket.default.id}"
+  bucket = aws_s3_bucket.default.id
 
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "AWSCloudTrailAclCheck20150319",
-            "Effect": "Allow",
-            "Principal": {"Service": "cloudtrail.amazonaws.com"},
-            "Action": "s3:GetBucketAcl",
-            "Resource": "${aws_s3_bucket.default.arn}"
-        },
-        {
-            "Sid": "AWSCloudTrailWrite20150319",
-            "Effect": "Allow",
-            "Principal": {"Service": "cloudtrail.amazonaws.com"},
-            "Action": "s3:PutObject",
-            "Resource": "${aws_s3_bucket.default.arn}/AWSLogs/${data.aws_caller_identity.current.account_id}/*",
-            "Condition": {"StringEquals": {"s3:x-amz-acl": "bucket-owner-full-control"}}
-        }
-    ]
-}
-EOF
+  policy = <<-EOF
+  {
+      "Version": "2012-10-17",
+      "Statement": [
+          {
+              "Sid": "AWSCloudTrailAclCheck20150319",
+              "Effect": "Allow",
+              "Principal": {"Service": "cloudtrail.amazonaws.com"},
+              "Action": "s3:GetBucketAcl",
+              "Resource": "${aws_s3_bucket.default.arn}"
+          },
+          {
+              "Sid": "AWSCloudTrailWrite20150319",
+              "Effect": "Allow",
+              "Principal": {"Service": "cloudtrail.amazonaws.com"},
+              "Action": "s3:PutObject",
+              "Resource": "${aws_s3_bucket.default.arn}/AWSLogs/${data.aws_caller_identity.current.account_id}/*",
+              "Condition": {"StringEquals": {"s3:x-amz-acl": "bucket-owner-full-control"}}
+          }
+      ]
+  }
+  EOF
 }
 
 resource "aws_iam_role" "cloudtrail_cloudwatch_events_role" {
   name_prefix        = "cloudtrail_events_role"
-  assume_role_policy = "${data.aws_iam_policy_document.assume_policy.json}"
+  assume_role_policy = data.aws_iam_policy_document.assume_policy.json
 }
 
 resource "aws_iam_role_policy" "policy" {
   name_prefix = "cloudtrail_cloudwatch_events_policy"
-  role        = "${aws_iam_role.cloudtrail_cloudwatch_events_role.id}"
-  policy      = "${data.aws_iam_policy_document.policy.json}"
+  role        = aws_iam_role.cloudtrail_cloudwatch_events_role.id
+  policy      = data.aws_iam_policy_document.policy.json
 }
 
 data "aws_iam_policy_document" "policy" {
@@ -114,13 +114,13 @@ resource "aws_cloudwatch_log_group" "default" {
 resource "aws_cloudtrail" "default" {
   name                          = "cloudtrail-${var.region}"
   enable_logging                = "true"
-  s3_bucket_name                = "${aws_s3_bucket.default.id}"
+  s3_bucket_name                = aws_s3_bucket.default.id
   enable_log_file_validation    = "false"
   is_multi_region_trail         = "true"
   include_global_service_events = "true"
-  cloud_watch_logs_role_arn     = "${aws_iam_role.cloudtrail_cloudwatch_events_role.arn}"
-  cloud_watch_logs_group_arn    = "${aws_cloudwatch_log_group.default.arn}"
-  depends_on                    = ["aws_s3_bucket_policy.default", "aws_iam_role_policy.policy"]
+  cloud_watch_logs_role_arn     = aws_iam_role.cloudtrail_cloudwatch_events_role.arn
+  cloud_watch_logs_group_arn    = aws_cloudwatch_log_group.default.arn
+  depends_on                    = [aws_s3_bucket_policy.default, aws_iam_role_policy.policy]
 
   # provisioner "local-exec" {
   #   command = "sleep 10"
@@ -128,13 +128,13 @@ resource "aws_cloudtrail" "default" {
 }
 
 output "sns_topic_arn" {
-  value = "${module.cloudtrail_api_alarms.sns_topic_arn}"
+  value = module.cloudtrail_api_alarms.sns_topic_arn
 }
 
 output "dashboard_individual" {
-  value = "${module.cloudtrail_api_alarms.dashboard_individual}"
+  value = module.cloudtrail_api_alarms.dashboard_individual
 }
 
 output "dashboard_combined" {
-  value = "${module.cloudtrail_api_alarms.dashboard_combined}"
+  value = module.cloudtrail_api_alarms.dashboard_combined
 }
--- main.tf
+++ main.tf
@@ -6,9 +6,9 @@ resource "aws_sns_topic" "default" {
 }
 
 resource "aws_sns_topic_policy" "default" {
-  count  = "${var.add_sns_policy != "true" && var.sns_topic_arn != "" ? 0 : 1}"
-  arn    = "${local.sns_topic_arn}"
-  policy = "${data.aws_iam_policy_document.sns_topic_policy.json}"
+  count  = var.add_sns_policy && length(var.sns_topic_arn) > 0 ? 0 : 1
+  arn    = local.sns_topic_arn
+  policy = data.aws_iam_policy_document.sns_topic_policy.json
 }
 
 data "aws_iam_policy_document" "sns_topic_policy" {
@@ -30,7 +30,7 @@ data "aws_iam_policy_document" "sns_topic_policy" {
     ]
 
     effect    = "Allow"
-    resources = ["${local.sns_topic_arn}"]
+    resources = [local.sns_topic_arn]
 
     principals {
       type        = "AWS"
@@ -50,7 +50,7 @@ data "aws_iam_policy_document" "sns_topic_policy" {
   statement {
     sid       = "Allow ${local.alert_for} CloudwatchEvents"
     actions   = ["sns:Publish"]
-    resources = ["${local.sns_topic_arn}"]
+    resources = [local.sns_topic_arn]
 
     principals {
       type        = "AWS"
@@ -60,7 +60,7 @@ data "aws_iam_policy_document" "sns_topic_policy" {
     condition {
       test     = "StringEquals"
       variable = "AWS:SourceArn"
-      values   = ["${aws_cloudwatch_metric_alarm.default.*.arn}"]
+      values   = aws_cloudwatch_metric_alarm.default.*.arn
     }
   }
 }
